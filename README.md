06da4719c106e0b6b054a36934cd7f1269e5c243
Tanish P <tanish.p@dibsy.one>
2022-04-02 14:53:07 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practise Assignment 1 
+
+
---
+++ python/pa.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  2 14:38:35 2022
+
+@author: tanishpanjwani
+"""
+
+for i in range(1, 11): 
+    print(i)
